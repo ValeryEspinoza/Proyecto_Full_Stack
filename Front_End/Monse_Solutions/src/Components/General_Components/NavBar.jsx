be28c6f11f1661e1e0544a38afbe65ddc1673554
ValeryEspinoza <vespinoza@fwdcostarica.com>
2024-11-11 10:24:16 -0600

    4.1, En proceso

--- Front_End/Monse_Solutions/src/Components/General_Components/NavBar.jsx
+++ Front_End/Monse_Solutions/src/Components/General_Components/NavBar.jsx
@@ -1,3 +1,77 @@
+
+
+import "../../Styles/Components_Styles/Genaral_C_Styles/NavBar.css"
+import Container  from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Logo_Negro from '../../Img/Components_Img/Logo_Negrov.png'
+import { Link } from 'react-router-dom'
+
+export default function NavBar() {
+  return (
+    <>
+        {[ 'lg' ].map((expand) => (
+        <Navbar key={expand} expand={expand} className="Navbar">
+          <Container fluid>
+
+          <img
+              src= {Logo_Negro}
+              width="auto"
+              height="80"
+
+            />
+            <Navbar.Brand href="#"></Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+            <Nav>
+           <div className ='navbar-links'>
+            <Link to="/About"><p className='itemLinkNav'>About</p></Link>
+            <Link to="/Contact"><p className='itemLinkNav'>Contact</p></Link>
+            <Link to="/"><p className='itemLinkNav'>Home</p></Link>
+            <Link to="/Login"><p className='itemLinkNav'>LogIn</p></Link>
+           </div>
+  
+             </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
+  );
+}
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 import React from 'react'
 import "../../Styles/Components_Styles/Genaral_C_Styles/NavBar.css"
 import { Link } from 'react-router-dom'
@@ -16,4 +90,5 @@ export default function NavBar() {
     </nav>
 </div>
   )
-}
+} 
+*/  
--- Front_End/Monse_Solutions/src/Components/Home_Components/HomeFichaInformativa.jsx
+++ Front_End/Monse_Solutions/src/Components/Home_Components/HomeFichaInformativa.jsx
@@ -5,41 +5,41 @@ import "../../Styles/Components_Styles/Home_C_Styles/HomeFichaInformativa.css"
 function HomeFichaInformativa() {
   return (
     <div className='bodyFicha'>
-    <div class='ContenedorPrincipal'>
-    <div class='ContenedorFicha'>
-      <div class='ContenedorDos'>
-        <img class='imgTrabajoExterior' src="src/Img/Components_Img/TrabajoExterior.jpeg" alt="TrabajoExterior" />
+    <div className='ContenedorPrincipal'>
+    <div className='ContenedorFicha'>
+      <div className='ContenedorDos'>
+        <img className='imgTrabajoExterior' src="src/Img/Components_Img/TrabajoExterior.jpeg" alt="TrabajoExterior" />
       </div>
-      <div class='ContenedorUno'>
-        <h2 class='tituloFicha'>Ficha Informativa</h2>
-        <p class='textoUno'>
+      <div className='ContenedorUno'>
+        <h2 className='tituloFicha'>Ficha Informativa</h2>
+        <p className='textoUno'>
           Un texto es una composición de signos codificados en un sistema de escritura que forma
           una unidad de sentido. También es una composición de caracteres imprimibles (con grafema)
           generados por un algoritmo de cifrado que, aunque no tienen sentido para cualquier persona,
           sí puede ser descifrado por su destinatario original. En otras palabras, un texto es un entramado
           de signos con una intención comunicativa que adquiere sentido en determinado contexto.
         </p>
-        <p class='textoDos'>
+        <p className='textoDos'>
           Un texto es una composición de signos codificados en un sistema de escritura que forma
           una unidad de sentido.
         </p>
       </div>
     </div>
   
-    <div class='ContenedorFicha'>
-      <div class='ContenedorDos'>
-        <img class='imgTrabajoExterior' src="src/Img/Components_Img/TrabajoMadera.jpeg" alt="" />
+    <div className='ContenedorFicha'>
+      <div className='ContenedorDos'>
+        <img className='imgTrabajoExterior' src="src/Img/Components_Img/TrabajoMadera.jpeg" alt="" />
       </div>
-      <div class='ContenedorUno'>
-        <h2 class='tituloFicha'>Ficha Informativa</h2>
-        <p class='textoUno'>
+      <div className='ContenedorUno'>
+        <h2 className='tituloFicha'>Ficha Informativa</h2>
+        <p className='textoUno'>
           Un texto es una composición de signos codificados en un sistema de escritura que forma
           una unidad de sentido. También es una composición de caracteres imprimibles (con grafema)
           generados por un algoritmo de cifrado que, aunque no tienen sentido para cualquier persona,
           sí puede ser descifrado por su destinatario original. En otras palabras, un texto es un entramado
           de signos con una intención comunicativa que adquiere sentido en determinado contexto.
         </p>
-        <p class='textoDos'>
+        <p className='textoDos'>
           Un texto es una composición de signos codificados en un sistema de escritura que forma
           una unidad de sentido.
         </p>
