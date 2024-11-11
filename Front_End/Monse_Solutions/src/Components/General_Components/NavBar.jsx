e79af8eb206f888c9bb37b95d7723eb665d27b19
An-17 <apena@fwdcostarica.com>
2024-11-11 10:48:24 -0600

    Hola

--- Front_End/Monse_Solutions/src/Components/General_Components/NavBar.jsx
+++ Front_End/Monse_Solutions/src/Components/General_Components/NavBar.jsx
@@ -8,8 +8,8 @@ export default function NavBar() {
     <nav className='ContenedorNav'>
         <img className='logoNavBar' src="src\Img\Components_Img\Logo_negro.png" alt="" />
         <div className='ItemsContent'>
-              <Link to="/About"><p className='itemLinkNav'>About</p></Link>
-            <Link to="/Contact"><p className='itemLinkNav'>Contact</p></Link>
+            <Link to="/About"><p className='itemLinkNav'>About</p></Link>
+             <Link to="/Contact"><p className='itemLinkNav'>Contact</p></Link>
             <Link to="/"><p className='itemLinkNav'>Home</p></Link>
             <Link to="/Login"><p className='itemLinkNav'>LogIn</p></Link>
         </div>
