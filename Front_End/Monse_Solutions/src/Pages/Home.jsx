039162cd1ae09526ffbe39da6cad1ac46bd43709
An-17 <apena@fwdcostarica.com>
2024-11-12 12:00:29 -0600

    4.2 - 4.4 cambio en sliderpor Angie

--- Front_End/Monse_Solutions/src/Pages/Home.jsx
+++ Front_End/Monse_Solutions/src/Pages/Home.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import NavBar from '../Components/General_Components/NavBar'
 import Footer from '../Components/General_Components/Footer'
 import HomeFichaInformativa from '../Components/Home_Components/HomeFichaInformativa'
-import HomeServicesRamdom from '../Components/Home_Components/HomeServicesRamdom'
+import Home_Services from '../Components/Home_Components/Home_Services'
 import ControlledCarousel from '../Components/Home_Components/CarrucelIntroduccion'
+import Connect_with_us from '../Components/Home_Components/Connect_with_us'
 
 function Home() {
   return (
    <> 
       <NavBar />
       <ControlledCarousel />
-       <HomeFichaInformativa />
-        <HomeServicesRamdom />
+      <HomeFichaInformativa />
+      <Connect_with_us/>
+      <Home_Services/>
       <Footer />
    </>
   )
