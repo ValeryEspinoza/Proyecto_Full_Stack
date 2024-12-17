2890ac184f081bf9d1a9ac885e2b2f2b6dcc71cc
ValeryEspinoza <vespinoza@fwdcostarica.com>
2024-12-17 09:13:46 -0600

    Nuevo servicio para reviews- Valery

--- Front_End/Monse_Solutions/src/Services/Get/GetServices.jsx
+++ Front_End/Monse_Solutions/src/Services/Get/GetServices.jsx
@@ -1,7 +1,6 @@
 
 async function GetServices() {
     try {
-        const token = localStorage.getItem('accessToken');
         const response = await fetch(`http://192.168.1.87:8000/api/services`);
         const data = await response.json();
         console.log(data);
